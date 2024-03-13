a5bdc9082710f0b017f984031e434978a60c452c
root <root@ip-172-31-40-141.ap-south-1.compute.internal>
2024-03-13 05:38:11 +0000

    file updated

--- demo.java
+++ demo.java
@@ -3,6 +3,9 @@ class Demo
      public static void main(...)
      {
          s.o.print(" Hello World!"); 
-         s.o.print(" Hello World!"); 
+         s.o.print(" Hello World!");
+	 s.o.print("this is Reshma ");
+	 s.o. print("this is soujanya");
+
      }
   }
